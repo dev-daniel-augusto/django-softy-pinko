8ed54a5bb69729cfb3d8722f85b5779c273be9bb
Diz <daniel.augusto.tonetti@gmail.com>
2021-04-16 14:12:28 -0300

    Update the admin file

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import FeatureSmall, \
+    FeatureBig, \
+    WorkProcess, \
+    Testimonial, \
+    Pricing, \
+    Counter
 
-# Register your models here.
+
+@admin.register(FeatureSmall)
+class FeatureSmallAdmin(admin.ModelAdmin):
+    list_display = ['feature_title', 'created', 'modified', 'is_live']
+
+
+@admin.register(FeatureBig)
+class FeatureBigAdmin(admin.ModelAdmin):
+    list_display = ['feature_title', 'created', 'modified', 'is_live']
+
+
+@admin.register(WorkProcess)
+class WorkProcessAdmin(admin.ModelAdmin):
+    list_display = ['process_name', 'created', 'modified', 'is_live']
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['author', 'created', 'modified', 'is_live']
+
+
+@admin.register(Counter)
+class COunterAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created', 'modified', 'is_live']
