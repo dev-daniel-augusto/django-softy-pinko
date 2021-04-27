6d234db64133f569a97ddcfea5f56fa86a180972
Diz <daniel.augusto.tonetti@gmail.com>
2021-04-27 20:29:31 -0300

    Add APIs views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import FormView
 from django.contrib import messages
 from django.urls import reverse_lazy
+from rest_framework import viewsets
 from .forms import ContactUsForm
 from .models import (
                     FeatureSmall,
@@ -10,6 +11,14 @@ from .models import (
                     Pricing,
                     Counter,
                     )
+from.serializers import (
+                        FeatureSmallSerializer,
+                        FeatureBigSerializer,
+                        WorkProcessSerializer,
+                        TestimonialSerializer,
+                        PricingSerializer,
+                        CounterSerializer,
+                        )
 
 
 class IndexView(FormView):
@@ -22,7 +31,7 @@ class IndexView(FormView):
         context['features_small'] = FeatureSmall.objects.all()
         context['features_big'] = FeatureBig.objects.all()
         context['work_process'] = WorkProcess.objects.all()
-        context['pricing'] = Pricing.objects.all()
+        context['pricing'] = Pricing.objects.order_by('price').all()
         context['testimonials'] = Testimonial.objects.all()
         context['counter'] = Counter.objects.all()
         return context
@@ -35,3 +44,33 @@ class IndexView(FormView):
     def form_invalid(self, form, *args, **kwargs):
         messages.error(self.request, "It was not possible to send your e-mail, please try again")
         return super(IndexView, self).form_invalid(form, *args, **kwargs)
+
+
+class FeatureSmallViewSet(viewsets.ModelViewSet):
+    queryset = FeatureSmall.objects.all()
+    serializer_class = FeatureSmallSerializer
+
+
+class FeatureBigViewSet(viewsets.ModelViewSet):
+    queryset = FeatureBig.objects.all()
+    serializer_class = FeatureBigSerializer
+
+
+class WorkProcessViewSet(viewsets.ModelViewSet):
+    queryset = WorkProcess.objects.all()
+    serializer_class = WorkProcessSerializer
+
+
+class TestimonialViewSet(viewsets.ModelViewSet):
+    queryset = Testimonial.objects.all()
+    serializer_class = TestimonialSerializer
+
+
+class PricingViewSet(viewsets.ModelViewSet):
+    queryset = Pricing.objects.all()
+    serializer_class = PricingSerializer
+
+
+class CounterViewSet(viewsets.ModelViewSet):
+    queryset = Counter.objects.all()
+    serializer_class = CounterSerializer
